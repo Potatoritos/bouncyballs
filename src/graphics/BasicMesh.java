3cb72bc25752f03c30a47ba9cd457549f948b5ac
Potatoritos <potatoritos@gmail.com>
2023-04-25 10:05:58 -0400

    Refactor BasicMesh

--- src/graphics/BasicMesh.java
+++ src/graphics/BasicMesh.java
@@ -7,46 +7,15 @@ import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class BasicMesh extends Mesh {
-    private final int vaoId;
-    private final int vertexVboId;
-    private final int indexVboId;
-    private final int vertexCount;
 
     public BasicMesh(float[] vertices, int[] indices) {
-        vertexCount = indices.length;
+        super();
 
-        vaoId = glGenVertexArrays();
-        glBindVertexArray(vaoId);
+        vao.bind();
 
-        vertexVboId = glGenBuffers();
-        FloatBuffer vertexBuffer = memAllocFloat(vertices.length);
-        vertexBuffer.put(vertices).flip();
-        glBindBuffer(GL_ARRAY_BUFFER, vertexVboId);
-        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-        memFree(vertexBuffer);
+        vao.createFloatVBO(0, 3, vertices);
+        vao.createEBO(indices);
 
-        indexVboId = glGenBuffers();
-        IntBuffer indexBuffer = memAllocInt(indices.length);
-        indexBuffer.put(indices).flip();
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVboId);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer, GL_STATIC_DRAW);
-        memFree(indexBuffer);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-    }
-    public void delete() {
-        glDeleteBuffers(vertexVboId);
-        glDeleteBuffers(indexVboId);
-
-        glDeleteVertexArrays(vaoId);
-    }
-    public int getVertexCount() {
-        return vertexCount;
-    }
-    public int getVaoId() {
-        return vaoId;
+        vao.unbind();
     }
 }

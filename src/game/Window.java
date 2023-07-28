ec6db1be3aa446715b60c867dff0286278cb8001
Potatoritos <potatoritos@gmail.com>
2023-07-27 21:10:53 -0400

    Add window icon

--- src/game/Window.java
+++ src/game/Window.java
@@ -12,6 +12,15 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL13C.GL_MULTISAMPLE;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.glfw.GLFWImage;
+import util.Util;
+
+import static org.lwjgl.stb.STBImage.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 
 public class Window {
     private long handle;
@@ -35,10 +44,65 @@ public class Window {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        handle = glfwCreateWindow(width, height, "bouncy balls", glfwGetPrimaryMonitor(), NULL);
-        isFullscreen = true;
+        handle = glfwCreateWindow(width, height, "bouncy balls", NULL, NULL);
+        isFullscreen = false;
         if (handle == NULL) {
             throw new RuntimeException("Failed to create the GLFW window");
+        }
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(handle, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    handle,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        // Set window icon
+        ByteBuffer icon16, icon32, icon64, icon128;
+        try {
+            icon16 = Util.ioResourceToByteBuffer("assets/images/icon16.png", 2048);
+            icon32 = Util.ioResourceToByteBuffer("assets/images/icon32.png", 4096);
+            icon64 = Util.ioResourceToByteBuffer("assets/images/icon64.png", 16384);
+            icon128 = Util.ioResourceToByteBuffer("assets/images/icon32.png", 65536);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        IntBuffer w = memAllocInt(1);
+        IntBuffer h = memAllocInt(1);
+        IntBuffer comp = memAllocInt(1);
+
+        try (GLFWImage.Buffer icons = GLFWImage.malloc(4)) {
+            ByteBuffer pixels16 = stbi_load_from_memory(icon16, w, h, comp, 4);
+            icons.position(0).width(w.get(0)).height(h.get(0)).pixels(pixels16);
+
+            ByteBuffer pixels32 = stbi_load_from_memory(icon32, w, h, comp, 4);
+            icons.position(1).width(w.get(0)).height(h.get(0)).pixels(pixels32);
+
+            ByteBuffer pixels64 = stbi_load_from_memory(icon64, w, h, comp, 4);
+            icons.position(2).width(w.get(0)).height(h.get(0)).pixels(pixels64);
+
+            ByteBuffer pixels128 = stbi_load_from_memory(icon128, w, h, comp, 4);
+            icons.position(3).width(w.get(0)).height(h.get(0)).pixels(pixels128);
+//
+            icons.position(0);
+            glfwSetWindowIcon(handle, icons);
+
+            stbi_image_free(pixels16);
+            stbi_image_free(pixels32);
+            stbi_image_free(pixels64);
+            stbi_image_free(pixels128);
         }
 
 
@@ -90,25 +154,6 @@ public class Window {
                 if (key == GLFW_KEY_F) {
                     isFullscreen = !isFullscreen;
                     glfwSetWindowMonitor(window, isFullscreen ? glfwGetPrimaryMonitor() : NULL, 0, 0, 1600, 900, GLFW_DONT_CARE);
-                    if (!isFullscreen) {
-                        try (MemoryStack stack = stackPush()) {
-                            IntBuffer pWidth = stack.mallocInt(1); // int*
-                            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-                            // Get the window size passed to glfwCreateWindow
-                            glfwGetWindowSize(handle, pWidth, pHeight);
-
-                            // Get the resolution of the primary monitor
-                            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-                            // Center the window
-                            glfwSetWindowPos(
-                                    handle,
-                                    (vidmode.width() - pWidth.get(0)) / 2,
-                                    (vidmode.height() - pHeight.get(0)) / 2
-                            );
-                        }
-                    }
                 }
             }
 //            System.out.printf("key=%s, scanCode=%s, action=%s, mods=%d\n", key, scanCode, action, mods);
--- src/util/Util.java
+++ src/util/Util.java
@@ -7,6 +7,22 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.*;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import org.lwjgl.BufferUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.lwjgl.BufferUtils.createByteBuffer;
 
 public class Util {
     /**
@@ -24,5 +40,77 @@ public class Util {
 
     public static Vector3f vector3dTo3f(Vector3d vector) {
         return new Vector3f((float)vector.x, (float)vector.y, (float)vector.z);
+    }
+
+    private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity) {
+        ByteBuffer newBuffer = createByteBuffer(newCapacity);
+        buffer.flip();
+        newBuffer.put(buffer);
+        return newBuffer;
+    }
+
+//    public static ByteBuffer ioResourceToByteBuffer(String resource, int bufferSize) throws IOException {
+//        ByteBuffer buffer;
+//        URL url = Thread.currentThread().getContextClassLoader().getResource(resource);
+//        if (url == null)
+//            throw new IOException("Classpath resource not found: " + resource);
+//        File file = new File(url.getFile());
+//        if (file.isFile()) {
+//            FileInputStream fis = new FileInputStream(file);
+//            FileChannel fc = fis.getChannel();
+//            buffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+//            fc.close();
+//            fis.close();
+//        } else {
+//            buffer = BufferUtils.createByteBuffer(bufferSize);
+//            InputStream source = url.openStream();
+//            if (source == null)
+//                throw new FileNotFoundException(resource);
+//            try {
+//                byte[] buf = new byte[8192];
+//                while (true) {
+//                    int bytes = source.read(buf, 0, buf.length);
+//                    if (bytes == -1)
+//                        break;
+//                    if (buffer.remaining() < bytes)
+//                        buffer = resizeBuffer(buffer, Math.max(buffer.capacity() * 2, buffer.capacity() - buffer.remaining() + bytes));
+//                    buffer.put(buf, 0, bytes);
+//                }
+//                buffer.flip();
+//            } finally {
+//                source.close();
+//            }
+//        }
+//        return buffer;
+//    }
+//
+    public static ByteBuffer ioResourceToByteBuffer(String resource, int bufferSize) throws IOException {
+        ByteBuffer buffer;
+
+        Path path = Paths.get(resource);
+        if (Files.isReadable(path)) {
+            try (SeekableByteChannel fc = Files.newByteChannel(path)) {
+                buffer = createByteBuffer((int)fc.size() + 1);
+                while (fc.read(buffer) != -1);
+            }
+        } else {
+            try (
+                    InputStream source = Util.class.getClassLoader().getResourceAsStream(resource);
+                    ReadableByteChannel rbc = Channels.newChannel(source)
+            ) {
+                buffer = createByteBuffer(bufferSize);
+
+                while (true) {
+                    int bytes = rbc.read(buffer);
+                    if (bytes == -1)
+                        break;
+                    if (buffer.remaining() == 0)
+                        buffer = resizeBuffer(buffer, buffer.capacity() * 2);
+                }
+            }
+        }
+
+        buffer.flip();
+        return buffer;
     }
 }
